0d4cd563a332bf5bf8864f5d093d0c9bdd7d2df7
Diogo Neves <diogo.neves@gmail.com>
2026-04-28 07:46:41 +0100

    Initial Commit

---
+++ Traces/TracesApp.swift
@@ -0,0 +1,17 @@
+//
+//  TracesApp.swift
+//  Traces
+//
+//  Created by Diogo Neves on 28/04/2026.
+//
+
+import SwiftUI
+
+@main
+struct TracesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ TracesTests/TracesTests.swift
@@ -0,0 +1,19 @@
+//
+//  TracesTests.swift
+//  TracesTests
+//
+//  Created by Diogo Neves on 28/04/2026.
+//
+
+import Testing
+@testable import Traces
+
+struct TracesTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+        // Swift Testing Documentation
+        // https://developer.apple.com/documentation/testing
+    }
+
+}
